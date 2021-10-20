ea1c74876e851b7692b261392a9f4a7454df26d0
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-10-20 12:00:38 +0200

    ajouts de routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import Vehicules from '../vehicules.json';
+var fs = require('fs');
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getTest(): string {
-    return this.appService.getTest();
+  getReturnAll(): object {
+    return Vehicules;
   }
+
+  @Get('/vehicules')
+  getReturnVehicules(): object {
+    return Vehicules;
+  }
+
+  @Get('/vehicules/:type')
+  getReturnVehiculesType(@Param() params): object {
+    return Vehicules.filter(vehicule => vehicule.type === params.type);
+  }
+
+  @Get('/vehicules/:type/:id')
+  getReturnVehiculesTypeId(@Param() params): object {
+    return Vehicules.filter(vehicule => (vehicule.type === params.type) && (vehicule.id === parseInt(params.id)));
+  }
+
+  @Post('/vehicules/:type/')
+  PostVehicules(@Body() body, @Param() param): void {
+     
+    fs.readFile('./vehicules.json', 'utf-8', function(err, data) {
+      if (err) throw err
+    
+      body.id = parseInt(body.id);
+      body.type = param.type
+      var arrayOfObjects = JSON.parse(data)
+      arrayOfObjects.push(body)
+    
+      console.log(arrayOfObjects)
+      fs.writeFileSync('./vehicules.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+         if (err) throw err
+      })
+    })
+  }
+
+  @Put('/vehicules/:type/:id')
+  PutVehicules(@Body() body, @Param() params): void {
+    
+  }
+
+ 
 }
